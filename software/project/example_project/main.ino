c39b7339bbb59c2d3dc0d44d5203459ac24270d0
ff1211 <767181989@qq.com>
2022-03-17 15:12:20 +0800

    add some file

--- software/project/example_project/main.ino
+++ software/project/example_project/main.ino
@@ -1,8 +1,22 @@
+//****************************************************************
+// Copyright 2022 Tianjin University TIM Lab. All Rights Reserved.
+//
+// File:
+// main.ino
+// 
+// Description:
+// Tim Arduino project example design.
+// 
+// Revision history:
+// Version  Date        Author      Changes      
+// 1.0      2022.03.17  Fanfei      Initial version
+//****************************************************************
+
 #include "../../arduino/tim_arduino.h"
 
 void setup() {
 // put your setup code here, to run once:
-
+    
 }
 
 void loop() {
