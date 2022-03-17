a5b7c8f476855a0850af2c2c43bf7e24e89ecba7
ff1211 <767181989@qq.com>
2022-03-17 15:14:55 +0800

    add some files

---
+++ software/module/sensor/sensor.h
@@ -0,0 +1,27 @@
+//****************************************************************
+// Copyright 2022 Tianjin University TIM Lab. All Rights Reserved.
+//
+// File:
+// sensor.h
+// 
+// Description:
+// Define sensor base class.
+// 
+// Revision history:
+// Version  Date        Author      Changes      
+// 1.0      2022.03.17  Fanfei      Initial version
+//****************************************************************
+
+#ifndef __SENSOR
+#define __SENSOR
+
+class sensor
+{
+private:
+    /* data */
+public:
+    sensor(/* args */);
+    ~sensor();
+};
+
+#endif
